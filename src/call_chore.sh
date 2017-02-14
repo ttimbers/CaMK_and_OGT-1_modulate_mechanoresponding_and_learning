8eb244c9aa45582ea77e0df44ad117937b45edc7
ttimbers <tiffany.timbers@gmail.com>
2017-02-14 14:06:36 -0800

    Added script to call chore to get all data in one file

--- src/call_chore.sh
+++ src/call_chore.sh
@@ -1,10 +1,23 @@
-## Set amount of memory to be devoted to running Choreography
-export MWT_JAVA_OPTIONS=-Xmx$3g
+## Written by: Tiffany A. Timbers
+## Feb 14, 2017
+##
+## This is the driver script will run Choreography to get a summary file of
+## reversals taps that occur 1s after a tap from each worm from data files from
+## the Multi-worm tracker (Swierczek et al., 2011).
+##
+## Requires the following input from the user:
+##
+##		$1: gigabytes of memory to be used to run Choreography (dependent upon
+##			the machine you are using
+##		$2: path to where data lives (all files should be in one folder - no subfolders)
+
+## Set amount of memory to be devoted to running Choreography (see README for how
+## Choreography should be installed to run this script)
+export MWT_JAVA_OPTIONS=-Xmx$1g
 
 ## call choreography to analyze the MWT data (each folder within the specified directory)
-for folder in */; do Chore --shadowless -p 0.027 -M 2 -t 20 -S -N all -o fDpesSlLwWaAmMkbPcdxyuvor1234 --plugin Reoutline::despike --plugin Respine --plugin MeasureReversal::all $folder; done
+for folder in $2/*; do Chore --shadowless --pixelsize 0.027 --minimum-move-body 2 --minimum-time 20 --segment --output speed,midline,morphwidth --plugin Reoutline::despike --plugin Respine --plugin MeasureReversal::all $folder; done
 
-## need to create a large file containing all data files with 
+## need to create a large file containing all rev files with
 ## data, plate name and strain name in each row
-##grep -r '[0-9]' $(find ./data -name '*.dat') > merged.file
-for filename in $(find . -name '*.dat'); do grep -H '[0-9]' $filename >> merged.file; done
+for filename in $(find . -name '*.rev'); do grep -H '[.]*' $filename >> data.srev; done
